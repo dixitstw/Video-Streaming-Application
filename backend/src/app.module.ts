db8cb46a793a6f4173218ad25d498bca07d32ca3
Dixit <dixitgautam01@gmail.com>
2025-05-12 19:21:26 +0545

    providers and middleware

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,57 @@
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, Schema } from '@nestjs/mongoose';
 import { secret } from './utils/constants';
 import { join } from 'path';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { User, UserSchema } from './model/user.schema';
+import { Video, VideoSchema } from './model/video.shcema';
+import { VideoController } from './controller/video.controller';
+import { UserController } from './controller/user.controller';
+import { VideoService } from './service/video.service';
+import { UserService } from './service/user.service';
+import { isAuthenticated } from './app.middleware';
 
 @Module({
 
   imports: [
     MongooseModule.forRoot('mongodb+srv://dixitgautam01:pdKUhHbR1m2jYRHa@cluster0.y59fys6.mongodb.net/'),
+
+    MongooseModule.forFeature([{name: User.name, schema: UserSchema}]),
+    
+    MongooseModule.forFeature([{name: Video.name, schema: VideoSchema}]),
+
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './public',
+        filename:(req, file, cb) => {
+          const ext = file.mimetype.split('/')[1];
+          cb(null, `${uuidv4()}-${Date.now()}.${ext}`)
+        }
+      })
+    }),
+
     JwtModule.register({
       secret,
-      signOptions: {expiresIn: '2h'}x
+      signOptions: {expiresIn: '2h'}
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
-    })
+    }),
   ],
+  controllers: [VideoController, UserController],
+  providers: [VideoService, UserService]
 })
-export class AppModule {}
+
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(isAuthenticated).exclude(
+       {path: '/video/:id', method: RequestMethod.GET }).forRoutes(VideoController);
+  }
+}
 
 
 
